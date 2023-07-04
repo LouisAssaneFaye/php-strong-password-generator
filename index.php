b6106b8233e83551c07099ed2e4c11851340b606
LouisAssaneFaye <louis.faye.18@outlook.it>
2023-07-04 19:52:29 +0200

    milestone 1 terminata

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
 <?php
 
+    $lenght = null;
+    if(empty($_GET['lenght'])){
+        echo 'select a lenght';
+    } else { 
+        $lenght = $_GET['lenght'];
+        $password = generateRandomPassword($_GET['lenght']);   
+    }
+
+    function generateRandomPassword($length) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[random_int(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+
 
 ?>
 
@@ -12,6 +30,19 @@
     <title>Document</title>
 </head>
 <body>
+    <form action="./index.php" method="GET">
+    <h1>
+        Insert the desired number of caracter of the password that you want generate
+    </h1>
+    <br>
+    <input type="number" name="lenght" id="lenght">
+    <input type="submit" value="check">
+    </form>  
+   <h2>
+       <?php
+           echo " Your generated password is : $password " ;
+       ?>
+   </h2>
     
 </body>
 </html>
